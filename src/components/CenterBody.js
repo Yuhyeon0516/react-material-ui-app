120ac04209e5718cccd0b91ac7240eb966c02b22
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-28 15:41:05 +0900

    CenterBody, Item Component 작성

--- src/components/CenterBody.js
+++ src/components/CenterBody.js
@@ -1,10 +1,15 @@
 import { Box } from "@mui/material";
 import React from "react";
+import Item from "./Item";
 
 const CenterBody = () => {
   return (
-    <Box flex={3} p={2} sx={{ backgroundColor: "primary.main" }}>
-      CenterBody
+    <Box flex={3} p={2}>
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+      <Item />
     </Box>
   );
 };
