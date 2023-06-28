6663bc71d43bd3dda84c2d0a10197665aaa3f624
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-28 15:43:33 +0900

    LeftBody 누락 부분 수정(ListItemButton 추가)

--- src/components/LeftBody.js
+++ src/components/LeftBody.js
@@ -1,5 +1,5 @@
 import { AddShoppingCart, Drafts, Home, Inbox } from "@mui/icons-material";
-import { Box, List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import React from "react";
 
 const LeftBody = () => {
@@ -8,28 +8,36 @@ const LeftBody = () => {
       <Box position="fixed">
         <List>
           <ListItem disablePadding>
-            <ListItemIcon>
-              <Home />
-            </ListItemIcon>
-            <ListItemText primary="Home" />
+            <ListItemButton>
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItemButton>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemIcon>
-              <Inbox />
-            </ListItemIcon>
-            <ListItemText primary="Inbox" />
+            <ListItemButton>
+              <ListItemIcon>
+                <Inbox />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemIcon>
-              <Drafts />
-            </ListItemIcon>
-            <ListItemText primary="Drafts" />
+            <ListItemButton>
+              <ListItemIcon>
+                <Drafts />
+              </ListItemIcon>
+              <ListItemText primary="Drafts" />
+            </ListItemButton>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemIcon>
-              <AddShoppingCart />
-            </ListItemIcon>
-            <ListItemText primary="Shopping" />
+            <ListItemButton>
+              <ListItemIcon>
+                <AddShoppingCart />
+              </ListItemIcon>
+              <ListItemText primary="Shopping" />
+            </ListItemButton>
           </ListItem>
         </List>
       </Box>
