2372dcb2c1b620d81ea26ffb29c7c4e09ab3ebd3
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-28 15:32:38 +0900

    LeftBody 기초 구성

--- src/components/LeftBody.js
+++ src/components/LeftBody.js
@@ -1,10 +1,38 @@
-import { Box } from "@mui/material";
+import { AddShoppingCart, Drafts, Home, Inbox } from "@mui/icons-material";
+import { Box, List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
 import React from "react";
 
 const LeftBody = () => {
   return (
-    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" }, backgroundColor: "primary.main" }}>
-      LeftBodyy
+    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position="fixed">
+        <List>
+          <ListItem disablePadding>
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemIcon>
+              <Inbox />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemIcon>
+              <Drafts />
+            </ListItemIcon>
+            <ListItemText primary="Drafts" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemIcon>
+              <AddShoppingCart />
+            </ListItemIcon>
+            <ListItemText primary="Shopping" />
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 };
