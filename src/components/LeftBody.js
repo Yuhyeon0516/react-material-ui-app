0ef321550dc81477367c42d5097e6b350b39a391
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-28 15:18:36 +0900

    Header 작성

--- src/components/LeftBody.js
+++ src/components/LeftBody.js
@@ -4,7 +4,7 @@ import React from "react";
 const LeftBody = () => {
   return (
     <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" }, backgroundColor: "primary.main" }}>
-      LeftBody
+      LeftBodyy
     </Box>
   );
 };
