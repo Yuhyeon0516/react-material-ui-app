bc08f90a47851c54cdd28c603fe3c5a9c7d1a7d7
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-28 13:20:03 +0900

    Material CSS Study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "@mui/material/Button";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import AccessibilityIcon from "@mui/icons-material/Accessibility";
+import styled from "@emotion/styled";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button size="large" variant="text">
+        Hello world
+      </Button>
+      <Button fullWidth={true} variant="contained">
+        Hello world
+      </Button>
+      <Button variant="outlined">Hello world</Button>
+      <br />
+      <Button startIcon={<AcUnitIcon />} variant="outlined">
+        Hello
+      </Button>
+      <Button endIcon={<AccessibilityIcon />} variant="outlined">
+        Hello
+      </Button>
+      <br />
+      <Button
+        sx={{
+          backgroundColor: "gray",
+          color: "white",
+          margin: 10,
+          "&:hover": {
+            backgroundColor: "black",
+          },
+          "&:disabled": {
+            backgroundColor: "white",
+            color: "black",
+          },
+        }}
+        variant="contained"
+      >
+        Custom
+      </Button>
+      <CustomeButton>Custom2</CustomeButton>
+      <br />
+      <Typography variant="h1" gutterBottom>
+        h1. Heading
+      </Typography>
     </div>
   );
 }
+
+const CustomeButton = styled(Button)({
+  backgroundColor: "red",
+  color: "white",
+  margin: 10,
+  "&:hover": {
+    backgroundColor: "orange",
+  },
+  "&:disabled": {
+    backgroundColor: "white",
+    color: "black",
+  },
+});
 
 export default App;
