e8033ce6057f807e534308e5888d04b32418c9a9
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-28 15:03:28 +0900

    Header Stack Footer 기본 구조 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Box, Stack } from "@mui/material";
 import "./App.css";
+import Header from "./components/Header";
+import LeftBody from "./components/LeftBody";
+import CenterBody from "./components/CenterBody";
+import RightBody from "./components/RightBody";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div></div>;
+  return (
+    <Box>
+      <Header />
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <LeftBody />
+        <CenterBody />
+        <RightBody />
+      </Stack>
+      <Footer />
+    </Box>
+  );
 }
 
 export default App;
